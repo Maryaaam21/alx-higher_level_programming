db0bfeba5858c3cbbdfeb077b34bff4d504c2cb7
Maryaaam21 <maryamhassan1821@gmail.com>
2024-05-21 01:56:33 -0700

    task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+let numOfArgs = 0;
+numOfArgs++;
+
+if (numOfArgs = 0) {
+    console.log('No argument');
+}
+if (numOfArgs = 1 ) {
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
+} 
